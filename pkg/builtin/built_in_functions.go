3649eccdc4047cd75a77579762ae4ee5e08437ae
therecluse26 <braddmagyar@gmail.com>
2020-03-08 14:52:26 -0400

    About to move to go modules

--- pkg/builtin/built_in_functions.go
+++ pkg/builtin/built_in_functions.go
@@ -6,27 +6,12 @@ import (
 
 type NotificationType string
 
-const (
-	NotificationNormal NotificationType = "normal"
-	NotificationSuccess = "success"
-	NotificationError = "error"
-)
-
 func Hello() string {
 	return "Hello, World!"
 }
 
-func Notify(message interface{}, notifyType ...interface{}) bool {
-	switch NotificationType(notifyType[0].(string)) {
-	case NotificationNormal:
-		beeep.Alert("" ,message.(string), "")
-	case NotificationSuccess:
-		beeep.Alert("Uranium Success" ,message.(string), "")
-	case NotificationError:
-		beeep.Alert("Uranium Error" ,message.(string), "")
-		return false
-	default:
-		beeep.Alert("" ,message.(string), "")
-	}
+func Notify(title interface{}, message interface{}) bool {
+
+	beeep.Alert(title.(string),message.(string), "")
 	return true
 }
